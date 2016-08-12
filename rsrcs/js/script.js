c6dc1979e4953c27eb510e4b2392e7099f90e06d
Sanchay Javeria <javeria2@illinois.edu>
2016-08-12 07:45:08 +0530

    animations

--- rsrcs/js/script.js
+++ rsrcs/js/script.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
+	document.querySelector(".js-animate-1").style.opacity = 0;
+	document.querySelector(".js-animate-2").style.opacity = 0;
+	document.querySelector(".js-frame").style.opacity = 0;
+
 	// sticky nav
 	$('.js-del-sec').waypoint(function(direction){
 		if(direction=="down") {
@@ -41,4 +45,41 @@ $(document).ready(function(){
 	  });
 	});
 
+	// animations
+	$(".js-animate-1").waypoint(function(direction){
+		$(".js-animate-1").addClass('animated fadeIn');
+		document.querySelector(".js-animate-1.animated").style.opacity = 1;
+	}, {
+		offset: '50%'
+	});
+	$(".js-frame").waypoint(function(direction){
+		$(".js-frame").addClass('animated fadeInUp');
+		document.querySelector(".js-frame").style.opacity = 1;
+	}, {
+		offset: '50%'
+	});
+	$(".js-animate-2").waypoint(function(direction){
+		$(".js-animate-2").addClass('animated fadeIn');
+		document.querySelector(".js-animate-2.animated").style.opacity = 1;
+	}, {
+		offset: '50%'
+	});
+	$(".js-animate-3").waypoint(function(direction){
+		$(".js-animate-3").addClass('animated pulse');
+	}, {
+		offset: '50%'
+	});
+
+	// burger nav
+	$('.burger-icon').click(function(){
+		if($('.burger-icon i').hasClass('fa-bars')){
+			$('.burger-icon i').addClass("fa-times");
+			$('.burger-icon i').removeClass("fa-bars");
+		} else {
+			$('.burger-icon i').removeClass("fa-times");
+			$('.burger-icon i').addClass("fa-bars");
+		}
+
+		$('.top-nav').slideToggle(400);
+	});
 });
